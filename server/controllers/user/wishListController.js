9a8ddc51126a36f11dac9286a29852ecf08011b9
navaf <navafsoft@gmail.com>
2025-05-03 20:00:45 +0530

    style: remove unnecessary comments for improved code clarity in product and wishlist controllers

--- server/controllers/user/wishListController.js
+++ server/controllers/user/wishListController.js
@@ -1,58 +1,52 @@
 import WishList from "../../models/wishlistSchema.js"
 import CustomError from "../../utils/customError.js"
 
-// to get the logged user's wishList
-const getUserWishList = async (req,res)=>{
-    // populate with product
-    const data = await WishList.findOne({userID:req.user.id}).populate("products")
-    if(data){
+const getUserWishList = async (req, res) => {
+    const data = await WishList.findOne({ userID: req.user.id }).populate("products")
+    if (data) {
         res.status(200).json(data)
-    }else{
-        // if no user ,will send an empty array
-        res.status(200).json({products:[]})
+    } else {
+        res.status(200).json({ products: [] })
     }
 }
 
-const addToWishList = async (req,res,next)=>{
-    const {productID} = req.body;
-    if(!productID){
-        return next(new CustomError("product is required",400))
+const addToWishList = async (req, res, next) => {
+    const { productID } = req.body;
+    if (!productID) {
+        return next(new CustomError("product is required", 400))
     }
 
-    // update wishList
     let newWishList = await WishList.findOneAndUpdate(
-        {userID:req.user.id},
-        {$addToSet:{products:productID}},
-        {new : true}       
-) 
-// if no wishList ,will create
-if(!newWishList){
-    newWishList = new WishList({
-        userID: req.user.id,
-        products:[productID ]
-    })
-    await newWishList.save()
-    return res.status(200).json({message:"added to wishlist"})
-}
-res.status(200).json(newWishList)
+        { userID: req.user.id },
+        { $addToSet: { products: productID } },
+        { new: true }
+    )
+    if (!newWishList) {
+        newWishList = new WishList({
+            userID: req.user.id,
+            products: [productID]
+        })
+        await newWishList.save()
+        return res.status(200).json({ message: "added to wishlist" })
+    }
+    res.status(200).json(newWishList)
 }
 
-// remove item from WishList
-const removeFromWishList = async(req,res,next)=>{
-    const {productID} = req.body;
-    if(!productID){
-        return next(new CustomError("product is required",400))
+const removeFromWishList = async (req, res, next) => {
+    const { productID } = req.body;
+    if (!productID) {
+        return next(new CustomError("product is required", 400))
     }
     const removeWishList = await WishList.findOneAndUpdate(
-        {userID : req.user.id},
-        {$pull:{products:productID}},
-        {new:true}
+        { userID: req.user.id },
+        { $pull: { products: productID } },
+        { new: true }
     )
-    if(removeWishList){
-        res.status(201).json({message:"removed from wishlist"})
-    }else{
-        next(new CustomError("product does not found in wishlist",404))
+    if (removeWishList) {
+        res.status(201).json({ message: "removed from wishlist" })
+    } else {
+        next(new CustomError("product does not found in wishlist", 404))
     }
 }
 
-export {getUserWishList,addToWishList,removeFromWishList}
+export { getUserWishList, addToWishList, removeFromWishList }
