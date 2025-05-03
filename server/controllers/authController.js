13a05379ba2d3542d0c6ce2741b6a795f5b95030
navaf <navafsoft@gmail.com>
2025-05-03 20:01:53 +0530

    style: remove unnecessary comments and improve code formatting in auth and public controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,18 +13,15 @@ const createRefreshToken = (id, role, expiresIn) => {
 };
 
 const userRegister = async (req, res, next) => {
-  //validating with joi
   const { value, error } = joiUserSchema.validate(req.body);
   const { name, email, password } = value;
   if (error) {
     return next(new CustomError(error.details[0].message, 400));
   }
-  // emailChecking
   const exists = await User.findOne({ email });
   if (exists) {
     return next(new CustomError("User already exists", 400));
   }
-  // password Hashing + salt
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({
     name,
@@ -32,7 +29,6 @@ const userRegister = async (req, res, next) => {
     password: hashedPassword,
   });
 
-  // adding user to db
   await newUser.save();
   res.json({ success: true, message: "user registered" });
 };
@@ -61,7 +57,6 @@ const loginUser = async (req, res, next) => {
       )
     );
   }
-  // creating token for logged user
   const token = createToken(user._id, user.role, "1h");
   const refreshToken = createRefreshToken(user._id, user.role, "1h");
 
@@ -97,13 +92,11 @@ const adminLogin = async (req, res, next) => {
     return next(new CustomError("Invalid credentials", 401));
   }
 
-  // creating token for logged admin
   const token = createToken(user._id, user.role, "1h");
   const refreshToken = createRefreshToken(user._id, user.role, "1d");
   user.refreshToken = refreshToken;
   await user.save();
 
-  //sending refreshToken to cookie
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true,
@@ -120,7 +113,6 @@ const adminLogin = async (req, res, next) => {
   res.json({ message: "Admin successfully logged in", token, currentUser });
 };
 
-// controller to handle refresh
 const refreshingToken = async (req, res, next) => {
   if (!req.cookies) {
     return next(new CustomError("No cookies found", 401));
@@ -140,7 +132,6 @@ const refreshingToken = async (req, res, next) => {
   res.status(200).json({ message: "Token refreshed", token: accessToken });
 };
 
-// controller to handle logout
 const logout = async (req, res, next) => {
   res.clearCookie("refreshToken", {
     httpOnly: true,
--- server/controllers/publicController.js
+++ server/controllers/publicController.js
@@ -1,15 +1,15 @@
 import Products from "../models/productsSchema.js";
 
-// to fetch all the products
+
 const allProducts = async (req, res) => {
-  const product = await Products.find({isDeleted:false });
+  const product = await Products.find({ isDeleted: false });
   if (!product) {
     return res.status(204).json({ message: "no item in products" });
   }
   res.status(200).json({ data: product });
 };
 
-// get the product by id
+
 const getProductById = async (req, res) => {
   const singleProduct = await Products.findById(req.params.id);
   if (!singleProduct) {
@@ -18,11 +18,11 @@ const getProductById = async (req, res) => {
   res.json(singleProduct);
 };
 
-// get the product by checking the category
+
 const getProductCategory = async (req, res) => {
   const productCategory = await Products.find({ type: req.params.type });
   if (!productCategory || productCategory.length === 0) {
-    return res.status(404).json({data: [], message: "category not found" });
+    return res.status(404).json({ data: [], message: "category not found" });
   }
   res.json({ data: productCategory, message: "category found" });
 };
